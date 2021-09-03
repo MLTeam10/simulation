a9eef1891f9841115d483634a9ca0080b5bccbed
FabrizioBilleciUNICT <fabrizio.billeci@studium.unict.it>
2021-09-03 18:16:30 +0200

    changed AR

--- ar.py
+++ ar.py
@@ -7,14 +7,182 @@ import torch
 import torchvision.transforms as T
 import numpy as np
 import cv2
-import random
 import warnings
 warnings.filterwarnings('ignore')
+
+
+def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
+    """
+    Calculates intersection over union
+
+    Parameters:
+        boxes_preds (tensor): Predictions of Bounding Boxes (BATCH_SIZE, 4)
+        boxes_labels (tensor): Correct labels of Bounding Boxes (BATCH_SIZE, 4)
+        box_format (str): midpoint/corners, if boxes (x,y,w,h) or (x1,y1,x2,y2)
+
+    Returns:
+        tensor: Intersection over union for all examples
+    """
+
+    if box_format == "midpoint":
+        box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+        box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+        box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
+        box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
+        box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+        box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+        box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
+        box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
+
+    if box_format == "corners":
+        box1_x1 = boxes_preds[..., 0:1]
+        box1_y1 = boxes_preds[..., 1:2]
+        box1_x2 = boxes_preds[..., 2:3]
+        box1_y2 = boxes_preds[..., 3:4]  # (N, 1)
+        box2_x1 = boxes_labels[..., 0:1]
+        box2_y1 = boxes_labels[..., 1:2]
+        box2_x2 = boxes_labels[..., 2:3]
+        box2_y2 = boxes_labels[..., 3:4]
+
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    # .clamp(0) is for the case when they do not intersect
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-6)
+
+
+def non_max_suppression(bboxes, iou_threshold, box_format="corners"):
+    """
+    Does Non Max Suppression given bboxes
+
+    Parameters:
+        bboxes (list): list of lists containing all bboxes with each bboxes
+        specified as [class_pred, prob_score, x1, y1, x2, y2]
+        iou_threshold (float): threshold where predicted bboxes is correct
+        threshold (float): threshold to remove predicted bboxes (independent of IoU) 
+        box_format (str): "midpoint" or "corners" used to specify bboxes
+
+    Returns:
+        list: bboxes after performing NMS given a specific IoU threshold
+    """
+
+    assert type(bboxes) == list
+
+    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
+    bboxes_after_nms = []
+
+    while bboxes:
+        chosen_box = bboxes.pop(0)
+
+        bboxes = [
+            box
+            for box in bboxes
+            if box[0] != chosen_box[0]
+            or intersection_over_union(
+                torch.tensor(chosen_box[2:6]),
+                torch.tensor(box[2:6]),
+                box_format=box_format,
+            )
+            < iou_threshold
+        ]
+
+        bboxes_after_nms.append(chosen_box)
+
+    return bboxes_after_nms
+
+
+def get_prediction(img, confidence):
+    """
+    get_prediction
+      parameters:
+        - img_path - path of the input image
+        - confidence - threshold to keep the prediction or not
+      method:
+        - Image is obtained from the image path
+        - the image is converted to image tensor using PyTorch's Transforms
+        - image is passed through the model to get the predictions
+        - masks, classes and bounding boxes are obtained from the model and soft masks are made binary(0 or 1) on masks
+          ie: eg. segment of cat is made 1 and rest of the image is made 0
+    
+    """
+
+    transform = T.Compose([T.ToTensor()])
+    img = transform(img)
+
+    pred = model([img])
+    pred_score = list(pred[0]['scores'].detach().cpu().numpy())
+
+    pred_t_scores = [pred_score.index(x) for x in pred_score if x > confidence]
+    
+    masks = (pred[0]['masks']>0.5).squeeze().detach().cpu().numpy()
+    classes = [CLASS_NAMES[i] for i in list(pred[0]['labels'].cpu().numpy())]
+    boxes = list(pred[0]['boxes'].detach().cpu().numpy())
+
+    bboxes = []
+    for i in range(0, len(pred_t_scores)):
+      k = pred_t_scores[i]
+      bboxes.append([classes[k], pred_score[k], boxes[k][0], boxes[k][1], boxes[k][2], boxes[k][3], masks[k]])
+
+    print(len(bboxes))
+
+    bboxes = non_max_suppression(bboxes, iou_threshold=0.1, box_format="corners")
+
+    print(len(bboxes))
+    
+    pred_boxes = [[(i[2], i[3]), (i[4], i[5])] for i in bboxes]
+    pred_masks = [i[6] for i in bboxes]
+    pred_class = [i[0] for i in bboxes]
+    pred_scores = [i[1] for i in bboxes]
+    return pred_masks, pred_boxes, pred_class, pred_scores
+
+
+def segment_instance(img, confidence, rect_th=2, text_size=1, text_th=2):
+    """
+    segment_instance
+      parameters:
+        - img_path - path to input image
+        - confidence- confidence to keep the prediction or not
+        - rect_th - rect thickness
+        - text_size
+        - text_th - text thickness
+      method:
+        - prediction is obtained by get_prediction
+        - each mask is given random color
+        - each mask is added to the image in the ration 1:0.8 with opencv
+        - final output is displayed
+    """
+    masks, boxes, pred_cls, pred_scores = get_prediction(img, confidence)
+
+    for i in range(len(masks)):
+      cv2.rectangle(img, (int(boxes[i][0][0]), int(boxes[i][0][1])), (int(boxes[i][1][0]), int(boxes[i][1][1])), color=(0, 255, 0), thickness=rect_th)
+      cv2.putText(
+        img, 
+        "%s %.2f" % (pred_cls[i], pred_scores[i]),
+        (int(boxes[i][0][0]), int(boxes[i][0][1])), 
+        cv2.FONT_HERSHEY_SIMPLEX, 
+        text_size, 
+        (0,255,0),
+        thickness=text_th
+      )
+    
+    return img
+
+
+
+##################################################################################################################################
+############################ MAIN ############################
+##################################################################################################################################
 
 # load the model
 model = torch.load('model.pt', map_location=torch.device('cpu')) # map_location for CPU usage
 model.eval()
-
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -33,113 +201,7 @@ args = vars(ap.parse_args())
 
 CLASS_NAMES = ['__background__', 'pedestrian']
 
-def get_coloured_mask(mask):
-    """
-    random_colour_masks
-      parameters:
-        - image - predicted masks
-      method:
-        - the masks of each predicted object is given random colour for visualization
-    """
-    colours = [[0, 255, 0],[0, 0, 255],[255, 0, 0],[0, 255, 255],[255, 255, 0],[255, 0, 255],[80, 70, 180],[250, 80, 190],[245, 145, 50],[70, 150, 250],[50, 190, 190]]
-    r = np.zeros_like(mask).astype(np.uint8)
-    g = np.zeros_like(mask).astype(np.uint8)
-    b = np.zeros_like(mask).astype(np.uint8)
-    r[mask == 1], g[mask == 1], b[mask == 1] = colours[random.randrange(0,10)]
-    coloured_mask = np.stack([r, g, b], axis=2)
-    return coloured_mask
-
-def get_prediction(img, confidence):
-    """
-    get_prediction
-      parameters:
-        - img_path - path of the input image
-        - confidence - threshold to keep the prediction or not
-      method:
-        - Image is obtained from the image path
-        - the image is converted to image tensor using PyTorch's Transforms
-        - image is passed through the model to get the predictions
-        - masks, classes and bounding boxes are obtained from the model and soft masks are made binary(0 or 1) on masks
-          ie: eg. segment of cat is made 1 and rest of the image is made 0
-    
-    """
-    #img = Image.open(img_path)
-    transform = T.Compose([T.ToTensor()])
-    img = transform(img)
-
-    #img = img.to(device)
-    pred = model([img])
-    pred_score = list(pred[0]['scores'].detach().cpu().numpy())
-    print(pred_score)
-
-    pred_t = 0
-
-    pred_t_scores = [pred_score.index(x) for x in pred_score if x > confidence]
-    if pred_t_scores:
-        pred_t = pred_t_scores[-1]
-
-    #pred_t = [pred_score.index(x) for x in pred_score if x>confidence][-1]
-
-    masks = (pred[0]['masks']>0.5).squeeze().detach().cpu().numpy()
-    # print(pred[0]['labels'].numpy().max())
-    pred_class = [CLASS_NAMES[i] for i in list(pred[0]['labels'].cpu().numpy())]
-    pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().cpu().numpy())]
-    masks = masks[:pred_t+1]
-    pred_boxes = pred_boxes[:pred_t+1]
-    pred_class = pred_class[:pred_t+1]
-    return masks, pred_boxes, pred_class
-
-from PIL import Image
-
-
-def segment_instance(img, confidence=0.1, rect_th=3, text_size=1, text_th=3):
-    """
-    segment_instance
-      parameters:
-        - img_path - path to input image
-        - confidence- confidence to keep the prediction or not
-        - rect_th - rect thickness
-        - text_size
-        - text_th - text thickness
-      method:
-        - prediction is obtained by get_prediction
-        - each mask is given random color
-        - each mask is added to the image in the ration 1:0.8 with opencv
-        - final output is displayed
-    """
-    masks, boxes, pred_cls = get_prediction(img, confidence)
-    #img = cv2.imread(img)
-    #img = Image.fromarray(img.astype(np.uint8))
-    img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2RGB)
-    for i in range(len(masks)):
-      #rgb_mask = get_coloured_mask(masks[i])
-      #img = cv2.addWeighted(img, 1, rgb_mask, 0.5, 0)
-      print(int(boxes[i][0][0]))
-
-      #cv2.rectangle(img, (100, 20), (0, 0), (255, 0, 0), 2)
-
-      #print(pred_cls[i], boxes[i][0])
-
-      cv2.rectangle(img, (int(boxes[i][0][0]), int(boxes[i][0][1])), (int(boxes[i][1][0]), int(boxes[i][1][1])), color=(0, 255, 0), thickness=rect_th)
-      cv2.putText(img,pred_cls[i], (int(boxes[i][0][0]), int(boxes[i][0][1])), cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th)
-    
-    return img
-
-# load the COCO class labels our Mask R-CNN was trained on
-#labelsPath = os.path.sep.join([args["mask_rcnn"], "object_detection_classes_coco.txt"])
-LABELS = ['__background__', 'pedestrian'] #open(labelsPath).read().strip().split("\n")
-# initialize a list of colors to represent each possible class label
 np.random.seed(42)
-
-# derive the paths to the Mask R-CNN weights and model configuration
-# weightsPath = os.path.sep.join([args["mask_rcnn"],
-# 	"frozen_inference_graph.pb"])
-# configPath = os.path.sep.join([args["mask_rcnn"],
-# 	"mask_rcnn_inception_v2_coco_2018_01_28.pbtxt"])
-# # load our Mask R-CNN trained on the COCO dataset (90 classes)
-# # from disk
-# print("[INFO] loading Mask R-CNN from disk...")
-# net = cv2.dnn.readNetFromTensorflow(weightsPath, configPath)
 
 # initialize the video stream and pointer to output video file
 vs = cv2.VideoCapture(args["input"])
@@ -168,13 +230,13 @@ while True:
     start = time.time()
     end = time.time()
     
-    frame = segment_instance(frame, 0.3)
+    frame = segment_instance(frame, args['confidence'])
 
     # check if the video writer is None
     if writer is None:
         # initialize our video writer
         fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-        writer = cv2.VideoWriter(args["output"], fourcc, 30, (frame.shape[1], frame.shape[0]), True)
+        writer = cv2.VideoWriter(args["output"], fourcc, 20, (frame.shape[1], frame.shape[0]), True)
     # write the output frame to disk
     writer.write(frame)
 
@@ -182,10 +244,3 @@ while True:
 print("[INFO] cleaning up...")
 writer.release()
 vs.release()
-
-
-
-
-
-
-
